3b1fe718736876e503fb877251ff4a52cb25a6b9
Kevin Peterson <kevinmpeterson@gmail.com>
2016-04-21 13:02:52 -0400

    LK and Farneback optical flow.

--- tracker/src/test_opencv.cpp
+++ tracker/src/test_opencv.cpp
@@ -23,6 +23,8 @@
 
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <tracker/load_images.h>
 
 int main(int argc, char** argv)
@@ -45,7 +47,35 @@ int main(int argc, char** argv)
 	}
 
 	cv::Mat currentFrame;
+	cv::Mat prevFrame;
+
+	cv::Mat currentFrameGray;
+	cv::Mat prevFrameGray;
+
+	cv::Mat displayImage;
+
+	cv::Mat flow, flowMag, flowAngle;
+	std::vector<cv::Mat> flowChannels;
+
+	std::vector<cv::Point2f> trackedPoints[2];
+	cv::Size subPixWinSize(10,10), winSize(31,31);
+	int numberOfTrackedPoints = 1000;
+	cv::TermCriteria termcrit(cv::TermCriteria::COUNT|cv::TermCriteria::EPS,20,0.03);
+
 	cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
+	cv::namedWindow("flow", cv::WINDOW_AUTOSIZE);
+
+	if(loader->framesAvailable())
+	{
+		if(!loader->getNextFrame(prevFrame))
+		{
+			std::cout << "Could not get first frame" << std::endl;
+			return -1;
+		}
+		cv::cvtColor(prevFrame, prevFrameGray, cv::COLOR_BGR2GRAY);
+		cv::goodFeaturesToTrack(prevFrameGray, trackedPoints[0], numberOfTrackedPoints, 0.01, 10, cv::Mat(), 3, 0, 0.04);
+		cv::cornerSubPix(prevFrameGray, trackedPoints[0], subPixWinSize, cv::Size(-1,-1), termcrit);
+	}
 
 	while(loader->framesAvailable())
 	{
@@ -55,12 +85,53 @@ int main(int argc, char** argv)
 			continue;
 		}
 
-		if(!currentFrame.data)
-			std::cout << "No data..." << std::endl;
+		if(!trackedPoints[0].empty())
+		{
+			cv::cvtColor(currentFrame, currentFrameGray, cv::COLOR_BGR2GRAY);
+
+			std::vector<uchar> status;
+	        std::vector<float> err;
+			cv::calcOpticalFlowPyrLK(
+				prevFrameGray, currentFrameGray, trackedPoints[0], trackedPoints[1],
+				status, err,
+				winSize, 3, termcrit, 0, 0.001);
+
+			currentFrame.copyTo(displayImage);
+			for(int ptI = 0; ptI < trackedPoints[1].size(); ++ptI)
+			{
+				if(!status[ptI])
+					continue;
+
+				cv::circle(displayImage, trackedPoints[1][ptI], 3, cv::Scalar(0, 255, 0), -1, 8);
+			}
+
+			trackedPoints[0] = trackedPoints[1];
+
+			cv::namedWindow("flow", cv::WINDOW_AUTOSIZE);
+			cv::imshow("flow", displayImage);
+		}
+
+
+
+#if 0
+		cv::calcOpticalFlowFarneback(prevFrameGray, currentFrameGray, flow, 0.125, 5, 5, 3, 5, 1.2, 0);
+
+		cv::split(flow, flowChannels);
+		cv::cartToPolar(flowChannels[0], flowChannels[1], flowMag, flowAngle);
+
+		//cv::normalize(flowMag, flowMag, 0, 2000, cv::NORM_MINMAX);
+
+		cv::namedWindow("flow", cv::WINDOW_AUTOSIZE);
+		cv::imshow("flow", flowMag*10000);
+#endif
 
 		cv::namedWindow("image", cv::WINDOW_AUTOSIZE);
 		cv::imshow("image", currentFrame);
-		cv::waitKey(10);
+
+		cv::waitKey(1);
+
+		currentFrame.copyTo(prevFrame);
+		currentFrameGray.copyTo(prevFrameGray);
 	}
 
 	std::cout << "Hello World" << std::endl;
